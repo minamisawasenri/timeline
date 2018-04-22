c5c1929c9b86f09a3ce502148ba8ebb9b5d4751b
Minamisawa Senri <info@minamisawasenri.com>
2018-04-22 23:19:37 +0100

    extracted events into their own js file, added a number of new events

---
+++ js/events.js
@@ -0,0 +1,420 @@
+var gameEvents = [
+  {
+    date: new Date('2008-04-25'),
+    category: ['game','port'],
+    entry: ['chaos;head'],
+    content: {
+      en:{
+        title:'Chaos;Head game released',
+        description:'The first game; where it all began. \
+          Chaos;Head was released on PC as a collaboration between Nitroplus and 5pb. \
+          A tale of delusions, deceit and despair; can you trust the reality which you see before you?',
+        youtube:'luKQwGMsH4w',
+        twitter: ['minamisawasenri'],
+      },
+    },
+  },{
+    date: new Date('2009-10-15'),
+    category: ['game','port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate game released',
+        description:'The current most popular entry in the series, Steins;Gate was originally console exclusive to Xbox 360. \
+          The story explores themes of time travel and the existence of world lines, along with the perils of trying to alter the past.',
+        youtube:'tiCNSLJ8Ihc',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2012-06-28'),
+    category: ['game','port'],
+    entry:['robotics;notes'],
+    content:{
+      en:{
+        title:'Robotics;Notes game released',
+        description:'Robotics;Notes - available at release on Playstation 3 and Xbox 360. \
+          A break from the normal 2d art, Robotics;Notes experimented with 3d models. \
+          Jumping further into the future, we learn more about robotics, augmented reality and realistic mechs. \
+          Just don\'t mention the monopoles.',
+        youtube:'PBoC2CDyy44',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2014-12-18'),
+    category: ['game','port'],
+    entry:['chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child game released',
+        description:'Chaos;Child was cursed from the start; it was originally only released on Xbox One so received poor sales. \
+          Handling the same themes as seen in Chaos;Head, Chaos;Child explores what happened after the events of the first game. \
+          The events which transpire make Chaos;Child my favourite entry in the series thusfar.',
+        youtube:'ooz3q7EAsQU',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2017-11-9'),
+    category: ['game','port'],
+    entry:['occultic;nine'],
+    content:{
+      en:{
+        title:'Occultic;Nine game released',
+        description:'For the first time, a game was produced after the original entry. \
+          The results were not great. \
+          Out on Playstation 4, Playstation Vita and Xbox One, the Occultic;Nine game failed to expand on the original material and characters. \
+          Concerns with the game were noted after release and a patch was promised with further content. \
+          At least we got some extreme box art.',
+        youtube:'VxP3N3IiOMw',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2017-03-30'),
+    category: ['game','port'],
+    entry:['chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child Love Chu☆Chu!! game released',
+        description:'Another fandisk (for Playstation 4 and Playstation Vita), CC LCC pushed the lewdity to a new level. \
+          What if the events of the original game failed to transpire and instead Takuru goes after the girls (and others)? \
+          Although the story was not as plot-relevant or engaging as CH LCC, it still had some memorable moments.',
+        youtube:'17JzYV59vkY',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2010-03-25'),
+    category: ['game','port'],
+    entry:['chaos;head'],
+    content:{
+      en:{
+        title:'Chaos;Head Love Chu☆Chu!! game released',
+        description:'The first fandisk (available on Xbox 360), CH LCC envelopes you in a world of positive delusions; normie encounters await. \
+          However, some moments take a darker turn and the story fits in well with the original (despite the routes being reasonably similar). \
+          We also have the joy of being introduced to Erin.',
+        youtube:'wwwNhAaEPjk',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2011-06-16'),
+    category: ['game','port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate Darling game released',
+        description:'',
+        youtube:'29uj77KJODw',
+      }
+    }
+  },{
+    date: new Date('2011-10-28'),
+    category: ['game','port'],
+    entry:['steins;gate','chaos;head'],
+    content:{
+      en:{
+        title:'Steins;Gate 8-bit game released',
+        youtube:'1iVkTECSbqI'
+      }
+    }
+  },{
+    date: new Date('2013-04-25'),
+    category: ['game','port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate Phenogram game released',
+        youtube:'75bR2b0o8qI'
+      }
+    }
+  },{
+    date: new Date('2015-12-10'),
+    category: ['game','port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate 0 game released',
+        youtube:'0getIXnq7xA'
+      }
+    }
+  },{
+    date: new Date('2009-02-26'),
+    category: ['game','port'],
+    entry:['chaos;head'],
+    content:{
+      en:{
+        title:'Chaos;Head Noah game released',
+        description:'Chaos;Head Noah (first released on Xbox 360) introduces a number of new endings for the original, for each of the main heroines. \
+          Fleshing out the characters helps you to grow more attached to them, before tearing them away with cruel fate.',
+        youtube:'m3KRSaSs7DQ',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2014-06-26'),
+    category: ['game','port'],
+    entry:['robotics;notes'],
+    content:{
+      en:{
+        title:'Robotics;Notes Elite game released',
+        description: 'Robotics;Notes Elite (released on Playstation Vita) added a number of animated scenes to the mix, as well as reworking the flow of others. \
+          The portable nature also meant the Vita could be used as a PokeCom, to view the world around you as in the game.',
+        twitter: ['minamisawasenri'],
+        youtube:'1kFIqlWq2Ts',
+      }
+    }
+  }
+];
+var portEvents = [
+  {
+    date: new Date('2014-03-31'),
+    category: ['port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate English release',
+      }
+    }
+  },{
+    date: new Date('2016-11-25'),
+    category: ['port'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate 0 English release',
+      }
+    }
+  },{
+    date: new Date('2017-10-13'),
+    category: ['port'],
+    entry:['chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child English released',
+      }
+    }
+  },
+];
+var animeEvents = [
+  {
+    date: new Date('2008-10-15'),
+    category: ['anime'],
+    entry:['chaos;head'],
+    content:{
+      en:{
+        title:'Chaos;Head anime begins',
+        description:'The Chaos;Head anime - a showcase of how to not adapt a visual novel. \
+          Some of the more memorable moments include the attack of the runaway construction machine and delusional insects, but on the other hand we did get Super Special. \
+          Unfortunately, many people discover Chaos;Head now through the anime.',
+        youtube:'j0adSLdS8M4',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2011-04-06'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate anime begins',
+        description:'',
+        youtube:'dd7BILZcYAY',
+      }
+    }
+  },{
+    date: new Date('2013-04-20'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate movie airs',
+        description:'',
+        youtube:'ZeKME9EhHT8',
+      }
+    }
+  },{
+    date: new Date('2012-10-12'),
+    category: ['anime'],
+    entry:['robotics;notes'],
+    content:{
+      en:{
+        title:'Robotics;Notes anime begins',
+        description:'',
+        youtube:'_Ffo_YZnfRA',
+      }
+    }
+  },{
+    date: new Date('2017-01-11'),
+    category: ['anime'],
+    entry:['chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child anime begins',
+        description:'',
+        youtube:'v2BN2gbJQlg',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },{
+    date: new Date('2016-10-09'),
+    category: ['anime'],
+    entry:['occultic;nine'],
+    content:{
+      en:{
+        title:'Occultic;Nine anime begins',
+        description:'',
+        youtube:'uYyUES_uif0',
+      }
+    }
+  },{
+    date: new Date('2018-04-12'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate 0 anime begins',
+        description:'',
+      }
+    }
+  },{
+    date: new Date('2012-02-22'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate America OVA airs',
+        description:'Set shortly after the events of the original series, the lab ends up flying to America where they are once more reunited with Kurisu.',
+        twitter: ['minamisawasenri'],
+        image:'img/a_sg_a.png'
+      }
+    }
+  },{
+    date: new Date('2015-12-03'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate alternative ending OVA airs',
+        description:'Released shortly before Steins;Gate 0, this episode helped to remind viewers of the events which occur and lead into 0. \
+          Most of the episode mirrors the original (until the heartbreaking branching point).',
+        twitter: ['minamisawasenri'],
+        image:'img/a_sg_ae.jpg'
+      }
+    }
+  },{
+    date: new Date('2014-10-15'),
+    category: ['anime'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate cognitive computing OVA begins',
+        description:'A tie in with IBM, this OVA introduces an AI upa which attempts to provide the lab with helpful cognitive predictions.',
+        twitter: ['minamisawasenri'],
+        image:'img/a_sg_cc.jpg'
+      }
+    }
+  },{
+    date: new Date('2017-01-11'),
+    category: ['anime'],
+    entry:['chaos;head','chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child Episode 0 OVA airs',
+        description:'This episode summarises the events of Chaos;Head, to provide some background for those starting at Chaos;Child. \
+          Although it was shortened, cutting out most of the characters and spoilers, the OVA did a good job at capturing the essence of the game.',
+        twitter: ['minamisawasenri'],
+        image:'img/a_cc_e0.png'
+      }
+    }
+  },{
+    date: new Date('2017-06-17'),
+    category: ['anime'],
+    entry:['chaos;child'],
+    content:{
+      en:{
+        title:'Chaos;Child Silent Sky OVA airs',
+        //description:'Covering the events of the true route of Chaos;Child',
+        image:'img/a_cc_ss.jpg'
+      }
+    }
+  }
+];
+var mangaEvents = [
+
+];
+var lightNovelEvents = [
+  {
+    date: new Date('2014-08-25'),
+    category: ['lightNovel'],
+    entry:['occultic;nine'],
+    content:{
+      en:{
+        title:'Occultic;Nine initial volume released',
+        description:'Taking a sidestep from the tradition of releasing new entries in game format, we have Occultic;Nine. \
+          Written by Chiyomaru himself, Occultic;Nine is the first SVN entry (unless the definition has been changed again). \
+          Looking into the science of the occult, the structure of the novel jumps between a number of characters and dates as you attempt to piece together the mystery.',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  }
+];
+var memeEvents = [
+  {
+    date: new Date('2016-02-19'),
+    category: ['meme'],
+    entry:['steins;gate'],
+    content:{
+      en:{
+        title:'Steins;Gate initial vape released',
+        description:'The vape for chosen intellectuals; an official Steins;Gate vape was released. The theme was based on Kurisu and the flavour of Dr Pepper.',
+        image:'img/m_sg_v.png',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  },
+  {
+    date: new Date('2014-04-01'),
+    category: ['meme'],
+    entry: ['other'],
+    content:{
+      en:{
+        title:'Specium;Boys/Spacium;Shonen announced',
+        description:'Originally an April Fools joke, the story of Specium;Boys hinted towards themes of aliens and conspiracy theories. \
+          However, there was a later announcement that the project would be produced under the title Spacium;Shonen. \
+          Ever since, there have been no updates regarding the project.',
+        image:'img/m_o_sb.jpg',
+        twitter: ['minamisawasenri'],
+      }
+    }
+  }
+];
+var universeEvents = [
+  {
+    date: new Date('2009-11-06'),
+    category: ['universe'],
+    entry:['chaos;head','chaos;child'],
+    content:{
+      en:{
+        title:'Shibuya Earthquake/Third Melt occurs',
+      }
+    }
+  }
+];
+var otherEvents = [
+  {
+    date: new Date('2018-04-25'),
+    category: ['other'],
+    entry:['chaos;head'],
+    content:{
+      en:{
+        title:'10 year anniversary of Chaos;Head release',
+        //this is a special event: for description, start with your twitter name then write your thoughts about the last 10 years
+        description:''
+      }
+    }
+  }
+];
+var events = [].concat(gameEvents,portEvents,animeEvents,mangaEvents,lightNovelEvents,memeEvents,universeEvents,otherEvents);
